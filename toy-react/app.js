445befcab5b664504fb0b05632ec895afd5b1264
itamarzil <itamarzil123@hotmail.com>
2021-03-23 16:39:36 +0200

    rehacked

---
+++ toy-react/app.js
@@ -0,0 +1,13 @@
+/** @jsx rehacked.h */
+/** @jsxFrag Re.Fragment */
+
+import Rehacked from "./rehacked";
+
+const Button = function () {
+  return (
+    <div>
+      {" "}
+      <button>click me</button>
+    </div>
+  );
+};
