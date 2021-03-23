8d9bc208ea5882a9926bcb675b4524847f7b8180
itamarzil <itamarzil123@hotmail.com>
2021-03-23 21:07:12 +0200

    done basic component renderning

--- toy-react/dist/main.js
+++ toy-react/dist/main.js
@@ -1,1 +1,45 @@
-(()=>{"use strict";const o={transform(o,n,e){"function"==typeof o?(console.log("transform is dealing with a component. calling it as a function"),console.log("the function is:",o),o()):(console.log("a:",o),document.createElement(o),console.log("element of type:",o," created")),console.log("b:",n),console.log("c:",e)},render(o,n){console.log("a:",o),console.log("b:",n)}};o.render(o.transform((function(){return o.transform("div",null,o.transform("button",null,"click me"))}),null),document.getElementById("root"))})();
+(() => {
+  "use strict";
+  const e = {
+    transform(e, o, n) {
+      if ("function" == typeof e) {
+        console.log(
+          "transform is dealing with a component. calling it as a function"
+        ),
+          console.log("the function is:", e);
+        let o = e();
+        return console.log("returning: ", o), o;
+      }
+      {
+        let t;
+        if (
+          (console.log("a:", e),
+          console.log("b:", o),
+          console.log("c:", n),
+          "div" === e || "button" === e
+            ? (console.log("creating div or button"),
+              (t = document.createElement(e)))
+            : (console.log("creating text node"),
+              (t = document.createTextNode(e)),
+              console.log("text node created:", t)),
+          n.nodeType)
+        )
+          t.appendChild(n);
+        else {
+          let e = document.createTextNode(n);
+          t.appendChild(e);
+        }
+        return console.log("returning: ", t), t;
+      }
+    },
+    render(e, o) {
+      o ? o.appendChild(e) : document.body.appendChild(e);
+    },
+  };
+  e.render(
+    e.transform(function () {
+      return e.transform("div", null, e.transform("button", null, "click me"));
+    }, null),
+    document.getElementById("root")
+  );
+})();
--- toy-react/rehacked.js
+++ toy-react/rehacked.js
@@ -6,19 +6,37 @@ export default {
       );
       console.log("the function is:", a);
       let res = a();
+      console.log("returning: ", res);
       return res;
     } else {
       console.log("a:", a);
-      let el = document.createElement(a);
-      console.log("element of type:", a, " created");
+      console.log("b:", b);
+      console.log("c:", c);
+      let el;
+      if (a === "div" || a === "button") {
+        console.log("creating div or button");
+        el = document.createElement(a);
+      } else {
+        console.log("creating text node");
+        el = document.createTextNode(a);
+        console.log("text node created:", el);
+      }
+      if (c.nodeType) {
+        el.appendChild(c);
+      } else {
+        let newTextNode = document.createTextNode(c);
+        el.appendChild(newTextNode);
+      }
+
+      console.log("returning: ", el);
       return el;
     }
-    console.log("b:", b);
-    console.log("c:", c);
   },
-
-  render(rehackedElement, container) {
-    let res = rehackedElement();
-    container.appendChildren(res);
+  render(node, container) {
+    if (container) {
+      container.appendChild(node);
+    } else {
+      document.body.appendChild(node);
+    }
   },
 };
