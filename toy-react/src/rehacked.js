20147ca2e4c8251704498adb372f8749ea3555f5
itamarzil <itamarzil123@hotmail.com>
2021-03-23 19:39:34 +0200

    started element creating

--- toy-react/src/rehacked.js
+++ toy-react/src/rehacked.js
@@ -1,6 +1,16 @@
 export default {
   transform(a, b, c) {
-    console.log("a:", a);
+    if (typeof a === "function") {
+      console.log(
+        "transform is dealing with a component. calling it as a function"
+      );
+      console.log("the function is:", a);
+      a();
+    } else {
+      console.log("a:", a);
+      document.createElement(a);
+      console.log("element of type:", a, " created");
+    }
     console.log("b:", b);
     console.log("c:", c);
   },
